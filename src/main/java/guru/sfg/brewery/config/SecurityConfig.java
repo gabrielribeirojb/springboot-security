39451ecdf90c644b1c0b61469ca486e1fd41499e
Gabriel Ribeiro <gaabriel.jrb@gmail.com>
2021-06-21 18:22:24 -0300

    In Memory Authentication Fluent API

--- src/main/java/guru/sfg/brewery/config/SecurityConfig.java
+++ src/main/java/guru/sfg/brewery/config/SecurityConfig.java
@@ -1,15 +1,11 @@
 package guru.sfg.brewery.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 /**
  * Created by jt on 6/13/20.
@@ -35,22 +31,34 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     }
     
     @Override
-    @Bean
-    protected UserDetailsService userDetailsService() {
-    	@SuppressWarnings("deprecation")
-		UserDetails admin = User.withDefaultPasswordEncoder()
-    			.username("spring")
-    			.password("guru")
-    			.roles("ADMIN")
-    			.build();
-    	
-    	 @SuppressWarnings("deprecation")
-		UserDetails user = User.withDefaultPasswordEncoder()
-                 .username("user")
-                 .password("password")
-                 .roles("USER")
-                 .build();
-
-         return new InMemoryUserDetailsManager(admin, user);
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception{
+    	auth.inMemoryAuthentication()
+    	.withUser("spring")
+    	.password("{noop}guru")
+    	.roles("ADMIN")
+    	.and()
+    	.withUser("user")
+    	.password("{noop}password")
+    	.roles("USER");
     }
+    
+//    @Override
+//    @Bean
+//    protected UserDetailsService userDetailsService() {
+//    	@SuppressWarnings("deprecation")
+//		UserDetails admin = User.withDefaultPasswordEncoder()
+//    			.username("spring")
+//    			.password("guru")
+//    			.roles("ADMIN")
+//    			.build();
+//    	
+//    	 @SuppressWarnings("deprecation")
+//		UserDetails user = User.withDefaultPasswordEncoder()
+//                 .username("user")
+//                 .password("password")
+//                 .roles("USER")
+//                 .build();
+//
+//         return new InMemoryUserDetailsManager(admin, user);
+//    }
 }
