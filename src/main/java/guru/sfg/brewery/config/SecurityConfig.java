e097bfc948bfafdead7658c60afed9b90b232696
Gabriel Ribeiro <gaabriel.jrb@gmail.com>
2021-06-22 17:39:05 -0300

    NoOp Password Encoder

--- src/main/java/guru/sfg/brewery/config/SecurityConfig.java
+++ src/main/java/guru/sfg/brewery/config/SecurityConfig.java
@@ -1,11 +1,14 @@
 package guru.sfg.brewery.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 /**
  * Created by jt on 6/13/20.
@@ -14,6 +17,11 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+	@Bean
+	PasswordEncoder passwordEncoder() {
+		return NoOpPasswordEncoder.getInstance();
+	}
+	
     @Override
     protected void configure(HttpSecurity http) throws Exception {
                 http
@@ -38,11 +46,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     	.roles("ADMIN")
     	.and()
     	.withUser("user")
-    	.password("{noop}password")
+    	.password("password")
     	.roles("USER")
     	.and()
     	.withUser("scott")
-    	.password("{noop}tiger")
+    	.password("tiger")
     	.roles("COSTUMER");
     }
     
