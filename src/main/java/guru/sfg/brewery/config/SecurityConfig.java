bbf07c3ffb45a1f11e0ed4e23312039fd75f3d06
Gabriel Ribeiro <gaabriel.jrb@gmail.com>
2021-06-23 19:34:53 -0300

    Spring Security Configuration

--- src/main/java/guru/sfg/brewery/config/SecurityConfig.java
+++ src/main/java/guru/sfg/brewery/config/SecurityConfig.java
@@ -3,12 +3,16 @@ package guru.sfg.brewery.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
+import guru.sfg.brewery.security.RestHeaderAuthFilter;
 import guru.sfg.brewery.security.SfgPasswordEncoderFactories;
 
 /**
@@ -18,8 +22,17 @@ import guru.sfg.brewery.security.SfgPasswordEncoderFactories;
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	
+    public RestHeaderAuthFilter restHeaderAuthFilter(AuthenticationManager authenticationManager){
+        RestHeaderAuthFilter filter = new RestHeaderAuthFilter(new AntPathRequestMatcher("/api/**"));
+        filter.setAuthenticationManager(authenticationManager);
+        return filter;
+    }
+	
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+    	 http.addFilterBefore(restHeaderAuthFilter(authenticationManager()),
+                 UsernamePasswordAuthenticationFilter.class);
+    	
                 http
                 .authorizeRequests(authorize -> {
                     authorize.antMatchers("/", "/webjars/**", "/login", "/resources/**").permitAll()
--- src/main/java/guru/sfg/brewery/security/RestHeaderAuthFilter.java
+++ src/main/java/guru/sfg/brewery/security/RestHeaderAuthFilter.java
@@ -17,7 +17,7 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class RestHeaderAuthFilter extends AbstractAuthenticationProcessingFilter{
 	
-	protected RestHeaderAuthFilter(RequestMatcher requiresAuthenticationRequestMatcher) {
+	public RestHeaderAuthFilter(RequestMatcher requiresAuthenticationRequestMatcher) {
 		super(requiresAuthenticationRequestMatcher);
 	}
 
