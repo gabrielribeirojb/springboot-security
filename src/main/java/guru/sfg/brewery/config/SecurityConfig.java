531c51ba2be360476306f343ceecb05d557d134e
Gabriel Ribeiro <gaabriel.jrb@gmail.com>
2021-06-23 18:18:48 -0300

    Delete Beer by ID MockMVC Test

--- src/main/java/guru/sfg/brewery/config/SecurityConfig.java
+++ src/main/java/guru/sfg/brewery/config/SecurityConfig.java
@@ -7,10 +7,7 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.crypto.password.StandardPasswordEncoder;
 
 import guru.sfg.brewery.security.SfgPasswordEncoderFactories;
 
--- src/test/java/guru/sfg/brewery/web/controllers/api/BeerRestControllerIT.java
+++ src/test/java/guru/sfg/brewery/web/controllers/api/BeerRestControllerIT.java
@@ -1,5 +1,6 @@
 package guru.sfg.brewery.web.controllers.api;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -13,6 +14,13 @@ import guru.sfg.brewery.web.controllers.BaseIT;
  */
 @WebMvcTest
 public class BeerRestControllerIT extends BaseIT {
+	
+	 @Test
+	    void deleteBeer() throws Exception {
+	        mockMvc.perform(delete("/api/v1/beer/97df0c39-90c4-4ae0-b663-453e8e19c311")
+	        .header("Api-Key", "spring").header("Api-Secret", "guru"))
+	        .andExpect(status().isOk());
+	    }
 
     @Test
     void findBeers() throws Exception{
